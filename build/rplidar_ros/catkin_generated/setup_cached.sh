c07b3d719b608d5e3d267c6fc5718fede2f35433
LSKpr <kacper.lisik@wp.pl>
2024-07-16 17:09:13 +0200

    added markers on the rviz map/spawning in the current position

--- build/rplidar_ros/catkin_generated/setup_cached.sh
+++ build/rplidar_ros/catkin_generated/setup_cached.sh
@@ -9,5 +9,6 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/jetbot/catkin_ws/devel/.private/rplidar_ros:$CMAKE_PREFIX_PATH"
 export PWD='/home/jetbot/catkin_ws/build/rplidar_ros'
+export PYTHONPATH="/home/jetbot/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/jetbot/catkin_ws/devel/.private/rplidar_ros/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/jetbot/catkin_ws/src/rplidar_ros:$ROS_PACKAGE_PATH"
--- src/vision_and_object_recognition/scripts/obj_recogn.py
+++ src/vision_and_object_recognition/scripts/obj_recogn.py
@@ -1,45 +1,90 @@
 #!/usr/bin/env python
 
-
 import rospy
-from std_msgs.msg import String
-from std_msgs.msg import String
-  
-def obj_sender_init():
-    pub = rospy.Publisher('obj_marker', String, queue_size=10)
-    rospy.init_node('obj_sender', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    
-  
-def obj_sender():
-   if not rospy.is_shutdown():
-        data_content = "hello wrd, obj_sender %s" % rospy.get_time()
-        rospy.loginfo(data_content)
-        pub.publish(data_content)
-        rate.sleep()
- 
-if __name__ == '__main__':
-    try:
-        obj_sender_init()
-        obj_sender()
-    except rospy.ROSInterruptException:
-        pass
+from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+import threading
 
+# Initialize global variables for position
+x, y = 0.0, 0.0
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    global x, y
     
-def listener():
+    # Extract position from the PoseStamped message
+    x = data.pose.position.x
+    y = data.pose.position.y
+    
+    rospy.loginfo("Received PoseStamped message:")
+    rospy.loginfo("Position - x: %f, y: %f", x, y)
 
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+def obj_sender():
+    # Initialize a new node named 'obj_sender'
+    pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=10)  # Create a Publisher
 
-    rospy.Subscriber("obj_marker", String, callback)
+    # Define the rate at which messages are published
+    rate = rospy.Rate(1)  # 1Hz
+    markerArray = MarkerArray()
+    count = 0
+    MARKERS_MAX = 100
+    while not rospy.is_shutdown():
+        # Create a Marker message
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.header.stamp = rospy.get_rostime()
+        marker.ns = "obj_name"
+        marker.id = count
+        marker.type = Marker.SPHERE  # sphere
+        marker.action = Marker.ADD
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.orientation.x = 0
+        marker.pose.orientation.y = 0
+        marker.pose.orientation.z = 0
+        marker.pose.orientation.w = 1.0
+        marker.scale.x = 0.5
+        marker.scale.y = 0.5
+        marker.scale.z = 0.5
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
 
-    # spin() simply keeps python from exiting until this node is stopped
+        rospy.loginfo("Publishing Marker message:")
+        if len(markerArray.markers) >= MARKERS_MAX:
+            markerArray.markers.pop(0)
+
+        markerArray.markers.append(marker)
+
+        # Renumber the marker IDs
+        for i, m in enumerate(markerArray.markers):
+           m.id = i
+    
+        # Publish the MarkerArray
+        pub.publish(markerArray)
+    
+        count += 1
+        rospy.loginfo(markerArray)
+        rate.sleep()
+
+def cord_listener():
+    # Subscribe to the `slam_out_pose` topic with `PoseStamped` message type
+    rospy.Subscriber("slam_out_pose", PoseStamped, callback)
+
+    # Keep Python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    try:
+        # Initialize the ROS node with a unique name
+        rospy.init_node('object_recognition_node', anonymous=True)
 
+        # Start the listener in a separate thread
+        listener_thread = threading.Thread(target=cord_listener)
+        listener_thread.start()
+
+        # Run the object sender function
+        obj_sender()
+    except rospy.ROSInterruptException:
+        pass
